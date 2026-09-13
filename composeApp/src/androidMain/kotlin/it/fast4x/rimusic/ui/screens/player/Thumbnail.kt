9a052ee2c36544381c23c3479b2c9f2cb895d278
jhynhb-ship-it <jhynhb@gmail.com>
2026-09-13 14:05:42 +0300

    Update Thumbnail.kt

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/ui/screens/player/Thumbnail.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/ui/screens/player/Thumbnail.kt
@@ -12,7 +12,7 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
@@ -29,20 +29,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.media3.common.MediaItem
 import androidx.media3.common.PlaybackException
 import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
-import coil.compose.rememberAsyncImagePainter
-import coil.request.ImageRequest
 import it.fast4x.rimusic.Database
 import it.fast4x.rimusic.LocalPlayerServiceBinder
 import it.fast4x.rimusic.R
@@ -59,7 +55,6 @@ import it.fast4x.rimusic.service.UnplayableException
 import it.fast4x.rimusic.service.VideoIdMismatchException
 import it.fast4x.rimusic.service.isLocal
 import it.fast4x.rimusic.ui.components.themed.SmartMessage
-import it.fast4x.rimusic.ui.components.themed.RotateThumbnailCoverAnimation
 import it.fast4x.rimusic.ui.styling.Dimensions
 import it.fast4x.rimusic.ui.styling.px
 import it.fast4x.rimusic.utils.DisposableListener
@@ -72,10 +67,8 @@ import it.fast4x.rimusic.utils.rememberPreference
 import it.fast4x.rimusic.utils.showCoverThumbnailAnimationKey
 import it.fast4x.rimusic.utils.showlyricsthumbnailKey
 import it.fast4x.rimusic.utils.showvisthumbnailKey
-import it.fast4x.rimusic.utils.thumbnail
 import it.fast4x.rimusic.utils.thumbnailTypeKey
 import it.fast4x.rimusic.utils.thumbnailpauseKey
-import it.fast4x.rimusic.colorPalette
 import it.fast4x.rimusic.thumbnailShape
 import timber.log.Timber
 import java.net.UnknownHostException
@@ -136,13 +129,8 @@ fun Thumbnail(
 
     val formatUnsupported = stringResource(R.string.error_file_unsupported_format)
 
-    var artImageAvailable by remember {
-        mutableStateOf(true)
-    }
-
     val clickLyricsText by rememberPreference(clickOnLyricsTextKey, true)
     var showvisthumbnail by rememberPreference(showvisthumbnailKey, false)
-    //var expandedlyrics by rememberPreference(expandedlyricsKey,false)
 
     player.DisposableListener {
         object : Player.Listener {
@@ -157,26 +145,13 @@ fun Thumbnail(
             override fun onPlayerError(playbackException: PlaybackException) {
                 error = playbackException
                 binder.stopRadio()
-                //context.stopService(context.intent<PlayerService>())
-                //context.stopService(context.intent<MyDownloadService>())
             }
         }
     }
 
     val window = nullableWindow ?: return
 
-    val coverPainter = rememberAsyncImagePainter(
-        model = ImageRequest.Builder(LocalContext.current)
-            .data(window.mediaItem.mediaMetadata.artworkUri.thumbnail(1200))
-            .size(coil.size.Size.ORIGINAL)
-            .build(),
-        onError = { artImageAvailable = false },
-        onSuccess = { artImageAvailable = true }
-    )
-
     val showCoverThumbnailAnimation by rememberPreference(showCoverThumbnailAnimationKey, false)
-    var coverThumbnailAnimation by rememberPreference(coverThumbnailAnimationKey, ThumbnailCoverType.Vinyl)
-
 
     AnimatedContent(
         targetState = window,
@@ -221,105 +196,43 @@ fun Thumbnail(
                     modifierUiType = modifier
                         .padding(vertical = 8.dp)
                         .aspectRatio(1f)
-                        //.size(thumbnailSizeDp)
                         .fillMaxSize()
-                        //.dropShadow(thumbnailShape(), colorPalette().overlay.copy(0.1f), 6.dp, 2.dp, 2.dp)
-                        //.dropShadow(thumbnailShape(), colorPalette().overlay.copy(0.1f), 6.dp, (-2).dp, (-2).dp)
                         .doubleShadowDrop(if (showCoverThumbnailAnimation) CircleShape else thumbnailShape(), 4.dp, 8.dp)
-                        //.clip(thumbnailShape())
                         .clip(if (showCoverThumbnailAnimation) CircleShape else thumbnailShape())
-                //.padding(14.dp)
                 else modifierUiType = modifier
                     .aspectRatio(1f)
-                    //.size(thumbnailSizeDp)
-                    //.padding(14.dp)
                     .fillMaxSize()
-                    //.clip(thumbnailShape())
                     .clip(if (showCoverThumbnailAnimation) CircleShape else thumbnailShape())
-
-
 
         Box(
             modifier = modifierUiType
         ) {
             if (showthumbnail) {
-                if ((!isShowingLyrics && !isShowingVisualizer) || (isShowingVisualizer && showvisthumbnail) || (isShowingLyrics && showlyricsthumbnail))
-                    if (artImageAvailable) {
-                        if (showCoverThumbnailAnimation)
-                            RotateThumbnailCoverAnimation(
-                                painter = coverPainter,
-                                isSongPlaying = player.isPlaying,
-                                modifier = Modifier
-                                    .pointerInput(Unit) {
-                                        detectTapGestures(
-                                            onLongPress = { onShowStatsForNerds(true) },
-                                            onTap = if (thumbnailTapEnabledKey) {
-                                                {
-                                                    onShowLyrics(true)
-                                                    onShowEqualizer(false)
-                                                }
-                                            } else null,
-                                            onDoubleTap = { onDoubleTap() }
-                                        )
+                if ((!isShowingLyrics && !isShowingVisualizer) || (isShowingVisualizer && showvisthumbnail) || (isShowingLyrics && showlyricsthumbnail)) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.Black)
+                            .pointerInput(Unit) {
+                                detectTapGestures(
+                                    onLongPress = { onShowStatsForNerds(true) },
+                                    onTap = if (thumbnailTapEnabledKey) {
+                                        {
+                                            onShowLyrics(true)
+                                            onShowEqualizer(false)
+                                        }
+                                    } else null,
+                                    onDoubleTap = { onDoubleTap() }
+                                )
+                            }
+                    )
+                }
 
-                                    },
-                                type = coverThumbnailAnimation
-                            )
-                        else
-                            Image (
-                                painter = coverPainter,
-                                contentDescription = null,
-                                contentScale = ContentScale.Fit,
-                                modifier = Modifier
-                                    .pointerInput(Unit) {
-                                        detectTapGestures(
-                                            onLongPress = { onShowStatsForNerds(true) },
-                                            onTap = if (thumbnailTapEnabledKey) {
-                                                {
-                                                    onShowLyrics(true)
-                                                    onShowEqualizer(false)
-                                                }
-                                            } else null,
-                                            onDoubleTap = { onDoubleTap() }
-                                        )
-
-                                    }
-                                    .fillMaxSize()
-                                    .clip(thumbnailShape())
-                            )
-
-                    } else {
-                        Image(
-                            painter = painterResource(R.drawable.ic_banner_foreground),
-                            colorFilter = ColorFilter.tint(colorPalette().accent),
-                            modifier = Modifier
-                                .pointerInput(Unit) {
-                                    detectTapGestures(
-                                        onLongPress = { onShowStatsForNerds(true) },
-                                        onTap = if (thumbnailTapEnabledKey) {
-                                            {
-                                                onShowLyrics(true)
-                                                onShowEqualizer(false)
-                                            }
-                                        } else null,
-                                        onDoubleTap = { onDoubleTap() }
-                                    )
-
-                                }
-                                .fillMaxSize()
-                                .clip(thumbnailShape()),
-                            contentDescription = "Background Image",
-                            contentScale = ContentScale.Fit
-                        )
-                    }
-
-                //if (!currentWindow.mediaItem.isLocal)
                 if (showlyricsthumbnail)
                     Lyrics(
                         mediaId = currentWindow.mediaItem.mediaId,
                         isDisplayed = isShowingLyrics && error == null,
                         onDismiss = {
-                            //if (thumbnailTapEnabledKey)
                             onShowLyrics(false)
                         },
                         ensureSongInserted = { Database.insert(currentWindow.mediaItem) },
@@ -362,32 +275,9 @@ fun Thumbnail(
                                 else -> unknownplaybackerror
                             }, PopupType.Error, context = context
                         )
-                    //    player.seekToNext()
                     } else errorCounter = 0
                 }
             }
-            /*
-            PlaybackError(
-                isDisplayed = error != null,
-                messageProvider = {
-                    if (currentWindow.mediaItem.isLocal) localMusicFileNotFoundError
-                    else when (error?.cause?.cause) {
-                        is UnresolvedAddressException, is UnknownHostException -> networkerror
-                        is PlayableFormatNotFoundException -> notfindplayableaudioformaterror
-                        is UnplayableException -> originalvideodeletederror
-                        is LoginRequiredException -> songnotplayabledueserverrestrictionerror
-                        is VideoIdMismatchException -> videoidmismatcherror
-                        is PlayableFormatNonSupported -> formatUnsupported
-                        else -> unknownplaybackerror
-                    }
-                },
-                onDismiss = {
-                    //player::prepare
-                    //player.stop()
-                    player.seekToNext()
-                }
-            )
-             */
         }
     }
 }
@@ -404,5 +294,4 @@ fun Modifier.thumbnailpause(
             scaleX = scale
             scaleY = scale
         }
-
 }
