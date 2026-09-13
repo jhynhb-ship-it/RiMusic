9d729bd189bb848cf825fc83af33ee359d9ec750
jhynhb-ship-it <jhynhb@gmail.com>
2026-09-13 15:08:22 +0300

    Update LayoutWithAdaptiveThumbnail.kt

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/ui/components/themed/LayoutWithAdaptiveThumbnail.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/ui/components/themed/LayoutWithAdaptiveThumbnail.kt
@@ -14,11 +14,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.media3.common.util.UnstableApi
-import coil.compose.AsyncImage
 import com.valentinilk.shimmer.shimmer
 import it.fast4x.rimusic.R
 import it.fast4x.rimusic.enums.PlayerThumbnailSize
@@ -27,7 +27,6 @@ import it.fast4x.rimusic.ui.styling.shimmer
 import it.fast4x.rimusic.utils.isLandscape
 import it.fast4x.rimusic.utils.playerThumbnailSizeKey
 import it.fast4x.rimusic.utils.rememberPreference
-import it.fast4x.rimusic.utils.thumbnail
 import it.fast4x.rimusic.colorPalette
 import it.fast4x.rimusic.thumbnailShape
 
@@ -65,7 +64,6 @@ fun adaptiveThumbnailContent(
         val playerThumbnailSize by rememberPreference(playerThumbnailSizeKey, PlayerThumbnailSize.Medium)
 
         val modifier = Modifier
-            //.padding(all = 16.dp)
             .padding(horizontal = playerThumbnailSize.size.dp)
             .padding(top = 16.dp)
             .clip(shape ?: thumbnailShape())
@@ -74,93 +72,6 @@ fun adaptiveThumbnailContent(
                     onClick()
                 }
             }
-            //.size(thumbnailSizeDp)
-
-        //val painter = rememberAsyncImagePainter(
-        //    model = url?.thumbnail(thumbnailSizePx),
-        //)
-
-        /*
-        val painter = rememberAsyncImagePainter(
-            model = ImageRequest.Builder(LocalContext.current)
-                .data(url)
-                .size(coil.size.Size.ORIGINAL)
-                .build()
-        )
-
-        var bitmap = remember<Bitmap?> {
-            null
-        }
-        val imageState = painter.state
-        */
-        /*
-        val scaledBitmap = remember {
-            bitmap?.asImageBitmap()?.let {
-                Bitmap.createScaledBitmap(
-                    it.asAndroidBitmap(),
-                    thumbnailSizePx,
-                    thumbnailSizePx,
-                    false
-                )
-                    //.asImageBitmap()
-            }
-        }
-         */
-
-        /*
-        var isExporting by remember {
-            mutableStateOf(false)
-        }
-
-        val exportLauncher =
-            rememberLauncherForActivityResult(ActivityResultContracts.CreateDocument("image/png")) { uri ->
-                if (uri == null) return@rememberLauncherForActivityResult
-
-                context.applicationContext.contentResolver.openOutputStream(uri)
-                    ?.use { outputStream ->
-                        if (imageState is AsyncImagePainter.State.Success) {
-                            bitmap = imageState.result.drawable.toBitmap()
-                            try {
-                                bitmap?.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
-                                outputStream.flush()
-                                outputStream.close()
-                            } catch (e: Exception) {
-                                e.printStackTrace()
-                                context.toast("Error")
-                            }
-                        } else context.toast("Error")
-                    }
-
-            }
-
-         */
-
-        /*
-        if (isExporting) {
-            InputTextDialog(
-                onDismiss = {
-                    isExporting = false
-                },
-                title = stringResource(R.string.enter_the_playlist_name),
-                value = "",
-                placeholder = stringResource(R.string.enter_the_playlist_name),
-                setValue = { text ->
-                    if(isExporting) {
-                        try {
-                            @SuppressLint("SimpleDateFormat")
-                            val dateFormat = SimpleDateFormat("yyyyMMddHHmmss")
-                            exportLauncher.launch("imageCover${text.take(20)}_${dateFormat.format(
-                                Date()
-                            )}")
-                        } catch (e: ActivityNotFoundException) {
-                            context.toast("Couldn't find an application to create documents")
-                        }
-                    }
-
-                }
-            )
-        }
-         */
 
         if (isLoading) {
             Spacer(
@@ -169,10 +80,9 @@ fun adaptiveThumbnailContent(
                     .background(colorPalette().shimmer)
             )
         } else {
-            AsyncImage(
-                model = url?.thumbnail(thumbnailSizePx),
-                contentDescription = null,
+            Box(
                 modifier = modifier
+                    .background(Color.Black)
             )
             if(showIcon)
                 onOtherVersionAvailable?.let {
@@ -191,31 +101,6 @@ fun adaptiveThumbnailContent(
                         )
                     }
                 }
-            /*
-            Box(
-                modifier = Modifier
-                    .align(Alignment.BottomEnd)
-                    .fillMaxWidth(0.2f)
-            ) {
-                HeaderIconButton(
-                    icon = R.drawable.image_download,
-                    color = colorPalette.text,
-                    onClick = {
-                        //isExporting = true
-                        try {
-                            @SuppressLint("SimpleDateFormat")
-                            val dateFormat = SimpleDateFormat("yyyyMMddHHmmss")
-                            exportLauncher.launch("ImageCover_${dateFormat.format(
-                                Date()
-                            )}")
-                        } catch (e: ActivityNotFoundException) {
-                            context.toast("Couldn't find an application to create documents")
-                        }
-                    },
-                    modifier = Modifier.size(35.dp)
-                )
-            }
-             */
         }
     }
 }
