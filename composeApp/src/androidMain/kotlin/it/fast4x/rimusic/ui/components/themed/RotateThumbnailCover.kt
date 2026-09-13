311fd0f7afd54bfcf62000e54ed457154c9dae98
jhynhb-ship-it <jhynhb@gmail.com>
2026-09-13 14:10:21 +0300

    Update RotateThumbnailCover.kt

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/ui/components/themed/RotateThumbnailCover.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/ui/components/themed/RotateThumbnailCover.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -39,14 +38,12 @@ import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import it.fast4x.rimusic.R
 import it.fast4x.rimusic.enums.ThumbnailCoverType
-import it.fast4x.rimusic.utils.VinylSizeKey
-import it.fast4x.rimusic.utils.rememberPreference
 
 @Composable
 fun RotateThumbnailCover(
     modifier: Modifier = Modifier,
     rotationDegrees: Float = 0f,
-    painter: Painter,
+    painter: Painter? = null,
     type: ThumbnailCoverType = ThumbnailCoverType.Vinyl,
     imageCoverSize : Float = 50f
 ) {
@@ -83,20 +80,20 @@ fun RotateThumbnailCover(
             .clip(roundedShape)
     ) {
 
-            Image(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .rotate(rotationDegrees)
-                    .aspectRatio(1.0f),
-                painter = painterResource(id = when (type) {
-                    ThumbnailCoverType.Vinyl -> R.drawable.vinyl_background
-                    ThumbnailCoverType.CD, ThumbnailCoverType.CDwithCover -> R.drawable.cd
-                }),
-                contentDescription = "disc background"
-            )
+        Image(
+            modifier = Modifier
+                .fillMaxSize()
+                .rotate(rotationDegrees)
+                .aspectRatio(1.0f),
+            painter = painterResource(id = when (type) {
+                ThumbnailCoverType.Vinyl -> R.drawable.vinyl_background
+                ThumbnailCoverType.CD, ThumbnailCoverType.CDwithCover -> R.drawable.cd
+            }),
+            contentDescription = "disc background"
+        )
 
         if (type in listOf(ThumbnailCoverType.Vinyl, ThumbnailCoverType.CDwithCover))
-            Image(
+            Box(
                 modifier = Modifier
                     .fillMaxSize(
                         when (type) {
@@ -104,14 +101,12 @@ fun RotateThumbnailCover(
                             ThumbnailCoverType.CD -> 1f
                             ThumbnailCoverType.CDwithCover -> 0.85f
                         }
-
                     )
                     .rotate(rotationDegrees)
                     .aspectRatio(1.0f)
                     .align(Alignment.Center)
-                    .clip(roundedShape),
-                painter = painter,
-                contentDescription = "song album cover"
+                    .clip(roundedShape)
+                    .background(Color.Black)
             )
 
         if (type == ThumbnailCoverType.CDwithCover) {
@@ -144,7 +139,7 @@ fun RotateThumbnailCover(
 fun RotateThumbnailCoverAnimation(
     modifier: Modifier = Modifier,
     isSongPlaying: Boolean = true,
-    painter: Painter,
+    painter: Painter? = null,
     type: ThumbnailCoverType = ThumbnailCoverType.Vinyl
 ) {
     var currentRotation by remember {
@@ -194,7 +189,7 @@ fun RotateThumbnailCoverAnimationModern(
     modifier: Modifier = Modifier,
     type: ThumbnailCoverType = ThumbnailCoverType.Vinyl,
     isSongPlaying: Boolean = true,
-    painter: Painter,
+    painter: Painter? = null,
     state : PagerState,
     it : Int,
     imageCoverSize : Float
